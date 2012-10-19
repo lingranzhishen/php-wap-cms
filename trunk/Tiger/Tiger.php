6ba9b05985c9a0aa9ed7297b0d1a2e8afd691812
74hucn@gmail.com <74hucn@gmail.com@bdf95cd2-751e-892e-327e-cf51065eec3e>
2012-10-19 07:53:28 +0000

    修改数据库类

--- trunk/Tiger/Tiger.php
+++ trunk/Tiger/Tiger.php
@@ -4,24 +4,37 @@
  *
  */
 
+$GLOBALS['_beginTime'] = microtime(true);
+
 if(version_compare(PHP_VERSION,'5.0.0','<') ) {
     die('require PHP 5.0+ ');
 }
 
-$GLOBALS['_beginTime'] = microtime(true);
 
-if(!defined('TIGER_PATH')) define('TIGER_PATH', dirname(__FILE__));
+if(!defined('TIGER_PATH')){
+    define('TIGER_PATH', dirname(__FILE__));
+}
 
-if(!defined('APP_PATH')) define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']));
+if(!defined('APP_PATH')){
+    define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']));
+}
 
-//TODO ?
+/*
+ * 引进系统类
+ */
+
+//引进数据库类
 include TIGER_PATH.'/db/DB.php';
+
+
 if(version_compare(PHP_VERSION,'5.2.0','<') ) {
 	include TIGER_PATH.'/common/compat.php';
 }
+
 //TODO ?
 
 // 记录加载文件时间
 $GLOBALS['_loadTime'] = microtime(true);
 
-echo $GLOBALS['_loadTime'] -$GLOBALS['_beginTime']."<br/>";
+//echo $GLOBALS['_loadTime'] -$GLOBALS['_beginTime']."<br/>";
+
--- trunk/Tiger/db/driver/db.php
+++ trunk/Tiger/db/driver/db.php
@@ -17,7 +17,7 @@ abstract class tiger_sql{
 	protected static $count = 0;
 
 	abstract protected function __construct();
-	
+
 	function __destruct() {
 		$this->close();
 		$this->db = null;
@@ -28,36 +28,36 @@ abstract class tiger_sql{
 
 	abstract protected function connect($host, $user, $pwd, $dbName);
 
-	abstract protected function selectDB ($name);
-	
+	abstract protected function selectDB ($dbName);
+
 	abstract protected function fetchArray();
 
 	abstract protected function execute($sql);
 
 	abstract protected function close();
-	
+
 	abstract protected function insertID();
 
 	abstract protected function setCharset($char);
-	
+
 	abstract protected function setCount($sql);
 
 	abstract protected function getRows();
 
 	abstract protected function getRowsX();
-	
+
 	abstract protected function getOne($sql);
 
 	abstract protected function getArray($sql);
-	
+
 	abstract protected function getArrayX($sql);
-	
-	abstract protected function fieldName($offest);
-	
+
+	abstract protected function fieldName();
+
 	abstract protected function pageArray($sql, $size, $page);
-	
+
 	abstract protected function pageArrayX($sql, $size, $page);
-	
+
 	protected function getCount() {
 		return $this->count;
 	}
